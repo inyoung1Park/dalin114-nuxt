50d04e7192953e7e199374baeef02bd614e9ccc9
박인영 <inyoung1.park@dongkuk.com>
2020-07-26 23:09:34 +0900

    main 중간

--- layouts/vuetify.vue
+++ layouts/vuetify.vue
@@ -21,27 +21,30 @@
   margin-bottom: 10px !important;
 }
 
+.app-bar {
+  left: 10px;
+  position: absolute;
+}
 .main-banner {
   width: auto;
   height: auto;
-  max-width: 130px;
-  max-height: 40px;
+  max-width: 10vw;
 }
 
+.side-app-bar {
+  right: 10px;
+  display: inline-block;
+}
 .main-call {
-  right: 100%;
   width: auto;
   height: auto;
-  max-width: 170px;
-  max-height: 55px;
+  max-width: 13vw;
 }
 
 .main-app {
-  right: 40%;
   width: auto;
   height: auto;
-  max-width: 155px;
-  max-height: 55px;
+  max-width: 12vw;
 }
 </style>
 
@@ -49,13 +52,7 @@
   <v-app dark>
     <v-navigation-drawer v-model="drawer" temporary absolute app>
       <v-list v-if="!$auth.loggedIn">
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -65,13 +62,7 @@
         </v-list-item>
       </v-list>
       <v-list v-if="$auth.loggedIn">
-        <v-list-item
-          v-for="(item, i) in userItems"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in userItems" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -81,52 +72,40 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar absolute id="app-bar" flat app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"> </v-app-bar-nav-icon>
-    
-      <v-toolbar-side-icon>
-        <v-img
-          class="main-banner"
-          :src="require('@/assets/icon/main_icon.png')"
-        >
-        </v-img>
-      </v-toolbar-side-icon>
+
+    <v-app-bar absolute class="app-bar" flat app>
+      <!--<v-app-bar-nav @click.stop="drawer = !drawer"></v-app-bar-nav> -->
+
+      <v-img
+        @click.stop="drawer = !drawer"
+        class="main-banner"
+        :src="require('@/assets/icon/main_icon.png')"
+      ></v-img>
 
       <v-spacer></v-spacer>
-     <v-btn icon @click.stop="fixed = !fixed">
+      <div class="side-app-bar">
         <v-img
+          @click.stop="clipped = !clipped"
           :right="right"
           class="main-call"
           :src="require('@/assets/icon/main_call.png')"
         ></v-img>
-      </v-btn> 
-      <v-btn icon @click.stop="clipped = !clipped">
+      </div>
+      <div>
         <v-img
+          @click.stop="clipped = !clipped"
           :right="right"
           class="main-app"
           :src="require('@/assets/icon/main_appDown.png')"
         ></v-img>
-      </v-btn>
-
-      <!-- <v-toolbar-title class="elevation-0">
-      </v-toolbar-title>-->
-      <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>-->
+      </div>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <!--
-      
-     <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-           </v-list-item>
-      </v-list>
-    </v-navigation-drawer>-->
+
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -145,37 +124,37 @@ export default {
         {
           icon: "mdi-apps",
           title: "홈",
-          to: "/"
+          to: "/",
         },
         {
           icon: "mdi-chart-bubble",
           title: "안내",
-          to: "/inspire"
+          to: "/inspire",
         },
         { icon: "mdi-chat-plus-outline", title: "게시판", to: "/articles/" },
         { icon: "mdi-heart-outline", title: "로그인", to: "/user/login" },
-        { icon: "mdi-charity", title: "회원가입", to: "/user/register" }
+        { icon: "mdi-charity", title: "회원가입", to: "/user/register" },
       ],
       userItems: [
         {
           icon: "mdi-apps",
           title: "홈",
-          to: "/"
+          to: "/",
         },
         {
           icon: "mdi-chart-bubble",
           title: "안내",
-          to: "/inspire"
+          to: "/inspire",
         },
         { icon: "mdi-chat-plus-outline", title: "게시판", to: "/articles/" },
         { icon: "mdi-heart-outline", title: "로그아웃", to: "/user/logout" },
-        { icon: "mdi-account", title: "내 정보", to: "/user/my-account" }
+        { icon: "mdi-account", title: "내 정보", to: "/user/my-account" },
       ],
       miniVariant: false,
       right: true,
-      rightDrawer: false
+      rightDrawer: false,
     };
-  }
+  },
   //if()
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fullpage-container">
     <div class="button-group">
-      <button type="button" :class="{ active: index == 0 }" @click="moveTo(0)">
+      <!-- <button type="button" :class="{ active: index == 0 }" @click="moveTo(0)">
         first page
       </button>
       <button type="button" :class="{ active: index == 1 }" @click="moveTo(1)">
@@ -16,61 +16,90 @@
         :key="btn"
         :class="{ active: index == btn + 2 }"
         @click="moveTo(btn + 2)"
-      >
+      > :class="{ active: index == 0 }"
         page {{ btn + 2 }}
-      </button>
-      <button type="button" @click="showPage()">add page</button>
+      </button> 
+      <button type="button" @click="showPage()">add page</button>-->
+
+      <v-img
+        :src="require('@/assets/icon/main_scroll.png')"
+        class="scroll-button"
+        @click="moveTo(index)"
+      ></v-img>
     </div>
-    <v-sheet id="fullpage-hide">
-      <div class="fullpage-wp" v-fullpage="opts" ref="fullpage">
-        <div class="page-1 page">
-          <h1 class="part-1" v-animate="{ value: 'bounceInLeft' }">
-            vue-fullpage.js
-          </h1>
-          <h3 class v-animate="{ value: 'bounceInLeft' }">
-            A sigle-page scroll plugin based on vue@2.x,support for mobile and
-            PC .
-          </h3>
-          <div>
-            <p class="part-1" v-animate="{ value: 'bounceInRight' }">
-              vue-fullpage
-            </p>
-          </div>
+    <div class="fullpage-wp" v-fullpage="opts" ref="fullpage">
+      <div class="page-1 page">
+        <!-- <h1 class="part-1" v-animate="{ value: 'bounceInLeft' }">vue-fullpage.js</h1>
+        <h3 class v-animate="{ value: 'bounceInLeft' }">
+          A sigle-page scroll plugin based on vue@2.x,support for mobile and PC
+          .
+        </h3>
+        <div>
+          <p class="part-1" v-animate="{ value: 'bounceInRight' }">vue-fullpage</p>
         </div>
-        <div class="page-2 page" id="hide-scroll">
-          <h2 class="part-2" v-animate="{ value: 'bounceInRight' }">
-            Easy to use plugin
-          </h2>
+        <div> 
+        open-on-hover</div>-->
+
+        <div class="d-flex justify-end align-end">
+          <v-card flat tile style="background: transparent;">
+            <v-menu
+              transition="scale-transition"
+              origin="center center"
+              v-for="(list, i) in btns"
+              :key="i"
+              rounded="0"
+              tile
+              offset-y
+              open-on-hover
+              @mouseup="detailMenu(list.title)"
+            >
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  text
+                  color="grey darken-2"
+                  class="white--text ma-8"
+                  v-bind="attrs"
+                  v-on="on"
+                >{{ list.title }}</v-btn>
+              </template>
+              <!-- -->
+              <v-list active-class="pink--text">
+                <v-list-item
+                  v-for="(detail, i) in menuItems"
+                  :key="i"
+                  :v-if="codeTitle == detail.title"
+                  link
+                >
+                  <v-list-item-title v-text="detail.title"></v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-card>
         </div>
-        <div class="page-3 page" id="hide-scroll">
-          <h2 class v-animate="{ value: 'bounceInTop' }">Working On Tablets</h2>
-          <h3 class v-animate="{ value: 'bounceInBotton' }">
-            Designed to fit different screen sizes as well as tablet and mobile
-            devices.
-          </h3>
-          <p class="part-3" v-animate="{ value: 'bounceInLeft', delay: 0 }">
-            vue-fullpage
-          </p>
-          <p class="part-3" v-animate="{ value: 'bounceInRight', delay: 300 }">
-            vue-fullpage
-          </p>
-          <p class="part-3" v-animate="{ value: 'bounceInDown', delay: 600 }">
-            vue-fullpage
-          </p>
-          <p class="part-3" v-animate="{ value: 'zoomInDown', delay: 900 }">
-            vue-fullpage
-          </p>
-        </div>
-        <div class="page-2 page" v-for="page in pageNum" :key="page">
-          <h2 class="part-2" v-animate="{ value: 'bounceInRight' }">
-            page {{ page }}
-          </h2>
+
+        <div class="page-1-main">
+          <v-img :src="require('@/assets/main/main_center.png')" class="main-center"></v-img>
         </div>
       </div>
-    </v-sheet>
+      <div class="page-2 page" id="hide-scroll">
+        <h2 class="part-2" v-animate="{ value: 'bounceInRight' }">Easy to use plugin</h2>
+      </div>
+      <div class="page-3 page" id="hide-scroll">
+        <h2 class v-animate="{ value: 'bounceInTop' }">Working On Tablets</h2>
+        <h3 class v-animate="{ value: 'bounceInBotton' }">
+          Designed to fit different screen sizes as well as tablet and mobile
+          devices.
+        </h3>
+        <p class="part-3" v-animate="{ value: 'bounceInLeft', delay: 0 }">vue-fullpage</p>
+        <p class="part-3" v-animate="{ value: 'bounceInRight', delay: 300 }">vue-fullpage</p>
+        <p class="part-3" v-animate="{ value: 'bounceInDown', delay: 600 }">vue-fullpage</p>
+        <p class="part-3" v-animate="{ value: 'zoomInDown', delay: 900 }">vue-fullpage</p>
+      </div>
+      <div class="page-2 page" v-for="page in pageNum" :key="page">
+        <h2 class="part-2" v-animate="{ value: 'bounceInRight' }">page {{ page }}</h2>
+      </div>
+    </div>
   </div>
-
-  
 </template>
 
 <script>
@@ -79,17 +108,18 @@ export default {
   layout: "vuetify",
   data() {
     return {
-      index: 0,
+      codeTitle: 0,
+      index: 1,
       pageNum: 0,
       opts: {
         start: 0,
         dir: "v",
         loop: false,
         duration: 300,
-        beforeChange: function(ele, current, next) {
+        beforeChange: function (ele, current, next) {
           this.index = next;
         },
-        afterChange: function(ele, current) {
+        afterChange: function (ele, current) {
           this.index = current;
 
           var con = document.getElementById("app-bar");
@@ -102,19 +132,62 @@ export default {
             // con.style.display = "block";
             //con.classList.add("inverted-scroll");
           }
-        }
-      }
+        },
+      },
+      btns: [
+        {
+          title: "작업사진",
+          code: 0,
+        },
+        {
+          title: "소개",
+          code: 1,
+        },
+        {
+          title: "배차신청",
+          code: 2,
+        },
+        {
+          title: "파트너등록",
+          code: 3,
+        },
+        {
+          title: "자주묻는질문",
+          code: 4,
+        },
+      ],
+      colors: ["deep-purple accent-4", "error", "teal darken-1"],
+      // items: [...Array(4)].map((_, i) => `Item ${i}`),
+      menuItems: [
+        {
+          title: "달인114 소개",
+          code: 1,
+        },
+        {
+          title: "소비자 서비스",
+          code: 1,
+        },
+        { title: "파트너 서비스", code: 1 },
+        { title: "신청게시판", code: 2 },
+        { title: "신청하기", code: 2 },
+      ],
     };
   },
   methods: {
-    moveTo: function(index) {
+    detailMenu(param) {
+      this.codeTitle = param;
+      console.log(this.codeTitle);
+      return param;
+    },
+    moveTo: function (index) {
+      this.index++;
       this.$refs.fullpage.$fullpage.moveTo(index, true);
     },
-    showPage: function() {
+    showPage: function () {
       this.pageNum++;
       this.$refs.fullpage.$fullpage.$update();
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -137,8 +210,7 @@ body {
   color: #eee;
 }
 .page-1 {
-  padding-top: 100px;
-  background: #1bbc9b;
+  background: #e8e8e8;
 }
 .page-2 {
   padding-top: 100px;
@@ -154,24 +226,36 @@ p {
 }
 .button-group {
   position: absolute;
-  top: 70px;
-  left: 30px;
+  text-align: center;
+  left: 47%;
+  bottom: 10px;
   z-index: 9;
 }
-.button-group button {
-  display: inline-block;
-  margin: 10px;
-  color: #000;
-  background: #fff;
-  background: rgba(255, 255, 255, 0.5);
-  -webkit-border-radius: 10px;
-  border-radius: 10px;
-  padding: 9px 18px;
-  border: none;
-  outline: none;
+
+.scroll-button {
+  width: 10%;
+  height: auto;
+  text-align: center;
 }
-.button-group button.active {
-  background: rgba(0, 0, 0, 0.5);
-  color: #fff;
+.page-1-main {
+  padding-top: 10vw;
+}
+.main-center {
+  position: absolute;
+  max-width: 55vw;
+  left: 20%;
+  height: auto;
+}
+
+.menu-list {
+  float: left;
+  width: auto;
+}
+.v-application .ma-8 {
+  margin: 0px !important;
+}
+
+.menu-list-detail {
+  background: url("~assets/main/main-menu.png") no-repeat center / cover;
 }
 </style>

acaf49b91c102e78ee396ed785bb118412bb5d9b
박인영 <inyoung1.park@dongkuk.com>
2020-08-09 22:30:09 +0900

    second paage

--- pages/index.vue
+++ pages/index.vue
@@ -104,10 +104,85 @@
           </div>
         </div>
       </div>
-      <div class="page-2 page" id="hide-scroll">
-        <h2 class="part-2" v-animate="{ value: 'bounceInRight' }">Easy to use plugin</h2>
+
+      <div class="page-2 page">
+        <v-navigation-drawer v-model="drawer" temporary absolute app>
+          <v-list v-if="!$auth.loggedIn">
+            <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-list v-if="$auth.loggedIn">
+            <v-list-item v-for="(item, i) in userItems" :key="i" :to="item.to" router exact>
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar absolute flat>
+          <!--<v-app-bar-nav @click.stop="drawer = !drawer"></v-app-bar-nav> -->
+
+          <v-img
+            @click="moveToHome()"
+            class="side-banner"
+            :src="require('@/assets/side/side-menu-home.png')"
+          ></v-img>
+
+          <v-spacer></v-spacer>
+
+          <v-img
+            @click.stop="drawer = !drawer"
+            class="side-banner"
+            :src="require('@/assets/side/side-menu-icon.png')"
+          ></v-img>
+        </v-app-bar>
+
+        <div class="page-2-main">
+          <div class="page-2-font">
+            <div class="page-2-font-detail">
+              <h2 style="color: #4386AF;">달인114</h2>
+              <h2>의 작업사진</h2>
+            </div>
+            <div class>
+              <v-img :src="require('@/assets/side/side-camera.png')" class="page-2-camera"></v-img>
+            </div>
+          </div>
+          <div>
+            <h3 style="color: grey; display:flex;">달인114와 함께한 작업현장을 확인해보세요!</h3>
+          </div>
+
+          <div class="page-2-grid">
+            <div style="display: flex;">
+              <v-img v-if="isToggle" :src="grid_1" @click="detailShow('grid-1')"></v-img>
+              <v-img :src="require('@/assets/side/grid-2.png')" @click="detailShow('grid-2')"></v-img>
+            </div>
+            <div style="display: flex;">
+              <v-img :src="require('@/assets/side/grid-3.png')" @click="detailShow('grid-3')"></v-img>
+              <v-img :src="require('@/assets/side/grid-4.png')" @click="detailShow('grid-4')"></v-img>
+            </div>
+          </div>
+        </div>
+
+        <div class="page-2-main-grid-detail">
+          <v-carousel cycle hide-delimiter-background show-arrows-on-hover>
+            <v-carousel-item v-for="(item, i) in cardList" :class="item.title" :key="i">
+              <div :v-if="item.code == cardNum"></div>
+            </v-carousel-item>
+          </v-carousel>
+        </div>
       </div>
-      <div class="page-3 page" id="hide-scroll">
+
+      <div class="page-3 page">
         <h2 class v-animate="{ value: 'bounceInTop' }">Working On Tablets</h2>
         <h3 class v-animate="{ value: 'bounceInBotton' }">
           Designed to fit different screen sizes as well as tablet and mobile
@@ -122,7 +197,7 @@
         <h2 class="part-2" v-animate="{ value: 'bounceInRight' }">page {{ page }}</h2>
       </div>
 
-      <div class="page-4 page" id="hide-scroll">
+      <div class="page-4 page">
         <h2 class v-animate="{ value: 'bounceInTop' }">Working On Tablets</h2>
         <h3 class v-animate="{ value: 'bounceInBotton' }">
           Designed to fit different screen sizes as well as tablet and mobile
@@ -133,7 +208,7 @@
         <p class="part-3" v-animate="{ value: 'bounceInDown', delay: 600 }">vue-fullpage</p>
         <p class="part-3" v-animate="{ value: 'zoomInDown', delay: 900 }">vue-fullpage</p>
       </div>
-      <div class="page-5 page" id="hide-scroll">
+      <div class="page-5 page">
         <h2 class v-animate="{ value: 'bounceInTop' }">Working On Tablets</h2>
         <h3 class v-animate="{ value: 'bounceInBotton' }">
           Designed to fit different screen sizes as well as tablet and mobile
@@ -144,7 +219,7 @@
         <p class="part-3" v-animate="{ value: 'bounceInDown', delay: 600 }">vue-fullpage</p>
         <p class="part-3" v-animate="{ value: 'zoomInDown', delay: 900 }">vue-fullpage</p>
       </div>
-      <div class="page-6 page" id="hide-scroll">
+      <div class="page-6 page">
         <h2 class v-animate="{ value: 'bounceInTop' }">Working On Tablets</h2>
         <h3 class v-animate="{ value: 'bounceInBotton' }">
           Designed to fit different screen sizes as well as tablet and mobile
@@ -155,7 +230,7 @@
         <p class="part-3" v-animate="{ value: 'bounceInDown', delay: 600 }">vue-fullpage</p>
         <p class="part-3" v-animate="{ value: 'zoomInDown', delay: 900 }">vue-fullpage</p>
       </div>
-      <div class="page-4 page" id="hide-scroll">
+      <div class="page-4 page">
         <h2 class v-animate="{ value: 'bounceInTop' }">Working On Tablets</h2>
         <h3 class v-animate="{ value: 'bounceInBotton' }">
           Designed to fit different screen sizes as well as tablet and mobile
@@ -179,25 +254,30 @@ export default {
       codeTitle: "intro",
       index: 1,
       pageNum: 0,
+      isToggle: true,
+      grid_1: require("@/assets/side/grid-1.png"),
+      grid_1_off: require("@/assets/side/grid-1-1.png"),
+      cardNum: "grid-1",
       opts: {
         start: 0,
         dir: "v",
         loop: false,
         duration: 300,
+        crollingSpeed: 300,
         beforeChange: function (ele, current, next) {},
         afterChange: function (ele, current) {},
       },
       menuLists: [
-        {
+        /* {
           title: "작업사진",
           code: "working",
-        },
+        },*/
         {
           title: "소개",
           code: "intro",
         },
         { title: "배차신청", code: "alloc" },
-        { title: "파트너등록", code: "partner" },
+        { title: "매칭게시판", code: "partner" },
       ],
       introLists: [
         {
@@ -212,6 +292,16 @@ export default {
           code: "alloc-1",
         },
         { title: "신청하기", code: "alloc-2" },
+      ],
+      cardList: [
+        {
+          title: "grid-1-1",
+          code: "grid-1",
+        },
+        {
+          title: "grid-1-2",
+          code: "grid-1",
+        },
       ],
       //여기서부터
       clipped: false,
@@ -281,6 +371,21 @@ export default {
       this.pageNum++;
       this.$refs.fullpage.$fullpage.$update();
     },
+    moveToHome: function () {
+      this.$refs.fullpage.$fullpage.moveTo(0, true);
+    },
+    detailShow(param) {
+      //this.grid_1 = require("@/assets/side/grid-1.png");
+      this.cardNum = param;
+
+      console.log("cardNum: " + this.cardNum);
+      if (this.cardNum == "grid-1" && this.isToggle == true) {
+        // document.getElementById("v-img").src = this.grid_1_off;
+        this.grid_1 = this.grid_1_off;
+      } else {
+        this.grid_1 = this.grid_1;
+      }
+    },
   },
 };
 </script>
@@ -313,7 +418,7 @@ body {
 }
 .page-2 {
   /*padding-top: 100px;*/
-  background-color: rgb(75, 191, 195);
+  background-color: #f0f9ff;
 }
 .page-3 {
   /*padding-top: 50px;*/
@@ -381,5 +486,58 @@ p {
 
 .menu-list-detail {
   background: url("~assets/main/main-menu.png") no-repeat center / cover;
+}
+
+.side-banner {
+  max-width: 2vw;
+  height: auto;
+}
+
+.page-2-main {
+  padding-top: 10vw;
+  position: absolute;
+  max-width: 70vw;
+  max-height: 100vw;
+  left: 10%;
+  display: inline;
+}
+
+.page-2-main-grid-detail {
+  padding-top: 15vw;
+  position: inherit;
+  max-width: 45vw;
+  max-height: auto;
+  left: 45vw;
+  display: flex;
+}
+.page-2-camera {
+  display: flex;
+  max-width: 4vw;
+  max-height: auto;
+}
+.page-2-font {
+  display: flex;
+  /*padding-top: 13px;
+  margin-right: 10px;*/
+}
+.page-2-grid {
+  width: 100%;
+  height: 100%;
+  max-width: 15vw;
+  max-height: auto;
+}
+
+.page-2-font-detail {
+  display: flex;
+  padding-top: 15px;
+  margin-right: 10px;
+}
+
+.grid-1-1 {
+  background: url("~assets/side/grid/1-1.jpg") no-repeat center / cover;
+}
+
+.grid-1-2 {
+  background: url("~assets/side/grid/1-2.jpg") no-repeat center / cover;
 }
 </style>
